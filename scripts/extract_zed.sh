ea48225d69a9907e446ac70b54ba30a04c059c8f
Daniel Korth <dkorth@student.ethz.ch>
2025-07-16 15:20:55 +0200

    add zed script

---
+++ scripts/extract_zed.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+FILE_NAME="new"
+INPUT_SVO_FILE="data/svo/$FILE_NAME.svo2"
+OUTPUT_PATH_DIR="data/zed/$FILE_NAME"
+
+
+# Video Extraction:
+mkdir -p $OUTPUT_PATH_DIR/images
+python src/zed/svo_export.py --mode 4 --input_svo_file $INPUT_SVO_FILE --output_path_dir $OUTPUT_PATH_DIR/images
+
+# Video Creation:
+python src/zed/merge_images_to_video_unified.py $OUTPUT_PATH_DIR/images -o $OUTPUT_PATH_DIR/video.mp4 --format mp4
+
+# # Tracking: 
+python src/zed/positional_tracking.py --input_svo_file $INPUT_SVO_FILE --output_file $OUTPUT_PATH_DIR/poses.txt
