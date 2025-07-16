836959220349ec805b924f14f30b1617da3dbac2
Daniel Korth <dkorth@student.ethz.ch>
2025-07-16 10:18:55 +0200

    add camera distortion

--- src/visualize_rerun.py
+++ src/visualize_rerun.py
@@ -1,10 +1,11 @@
 from pathlib import Path
 import argparse
 import rerun as rr
-from utils.zed import load_poses, load_camera_intrinsics, load_all_rgb_images, load_all_depth_images
+from utils.zed import load_poses, load_camera_intrinsics, load_all_rgb_images, load_all_depth_images, get_camera_matrix, get_distortion_coeffs
 from utils.rerun import setup_blueprint
 import numpy as np
 from scipy.spatial.transform import Rotation
+from utils.cv2_utils import unproject_image
 
 def main():
     # Parse command line arguments
@@ -25,15 +26,16 @@ def main():
     rr.set_time(timeline="world", sequence=0)
 
     intrinsics = load_camera_intrinsics("/local/home/dkorth/Projects/dynamic-scene-graphs/data/SN35693142.conf", camera="left", resolution="2K")
-    K = np.array([[intrinsics["fx"], 0, intrinsics["cx"]], [0, intrinsics["fy"], intrinsics["cy"]], [0, 0, 1]])
+    K = get_camera_matrix(intrinsics)
+    dist = get_distortion_coeffs(intrinsics)
 
     data_dir = Path(args.data_dir)
     rgb_images = load_all_rgb_images(data_dir / "images", max_frames=args.max_frames, subsample=args.subsample)
     depth_images = load_all_depth_images(data_dir / "images", max_frames=args.max_frames, subsample=args.subsample)
-    translations, rotations = load_poses(data_dir / "poses.txt", max_frames=args.max_frames, subsample=args.subsample)
+    tvecs, rvecs = load_poses(data_dir / "poses.txt", max_frames=args.max_frames, subsample=args.subsample)
 
     print(f"Loaded {len(rgb_images)} RGB images and {len(depth_images)} depth images")
-    print(f"Loaded {len(translations)} poses (translations: {len(translations)}, rotations: {len(rotations)})")
+    print(f"Loaded {len(tvecs)} poses (translations: {len(tvecs)}, rotations: {len(rvecs)})")
 
     rr.log("world/camera/image", rr.ViewCoordinates.RDF)
 
@@ -43,12 +45,16 @@ def main():
         focal_length=[intrinsics["fx"], intrinsics["fy"]],
     ))
 
-    for i, (rgb, depth, translation, rotation) in enumerate(zip(rgb_images, depth_images, translations, rotations)):
+    for i, (rgb, depth, tvec, rvec) in enumerate(zip(rgb_images, depth_images, tvecs, rvecs)):
         rr.log("world/camera/image/rgb", rr.Image(rgb, color_model=rr.ColorModel.RGB))
-        rr.log("world/camera/image/depth", rr.DepthImage(depth, meter=1000, depth_range=[0, 3000]))
+        # rr.log("world/camera/image/depth", rr.DepthImage(depth, meter=1000, depth_range=[0, 3000]))
+
+        points_3d, _ = unproject_image(depth, K, dist, rvec, tvec)
+        rr.log("world/points", rr.Points3D(points_3d, colors=[255, 0, 0]))
+
         rr.log("world/camera", rr.Transform3D(
-            rotation=rr.RotationAxisAngle(axis=rotation, angle=-float(np.linalg.norm(rotation))),
-            translation=-Rotation.from_rotvec(rotation).inv().apply(translation)
+            rotation=rr.RotationAxisAngle(axis=rvec, angle=-float(np.linalg.norm(rvec))),
+            translation=-Rotation.from_rotvec(rvec).inv().apply(tvec)
         ))
         rr.set_time(timeline="world", sequence=i)
 
